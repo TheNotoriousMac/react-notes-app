83481e82030110821b2ea097ea7dde567d9d8da3
TheNotoriousMac <damani.mccaskie@mycavehill.uwi.edu>
2022-09-13 10:17:40 -0400

    View, Add and Delete Note functionality added.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList";
 
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is my first note!",
+      date: "13/09/2022",
+    },
+    {
+      id: nanoid(),
+      text: "This is my second note!",
+      date: "13/09/2022",
+    },
+    {
+      id: nanoid(),
+      text: "This is my third note!",
+      date: "13/09/2022",
+    },
+    {
+      id: nanoid(),
+      text: "This is my fourth note!",
+      date: "13/09/2022",
+    },
+  ]);
+
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString()
+    };
+
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  };
+
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
+
   return (
     <div className="container">
-      <NotesList />
+      <NotesList notes={notes} handleAddNote={addNote} handleDeleteNote={deleteNote} />
     </div>
   );
 }
